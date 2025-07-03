7b5a833acace6077509e691681aba757ec525c46
sacrhamza <sacrhamzaschool@gmail.com>
2025-07-03 17:22:48 +0100

    automatic push (minishell hsacr) 2025-07-03/17/22

--- src/wild_card_expansion/wild_card_expansion.c
+++ src/wild_card_expansion/wild_card_expansion.c
@@ -3,22 +3,49 @@
 #include <stdbool.h>
 #include <dirent.h>
 #include "libft.h"
+char	get_last_char(char *str)
+{
+	size_t	index;
+
+	index = 0;
+	while (str[index])
+	{
+		if (str[index + 1] == '\0')
+			return (str[index]);
+		index++;
+	}
+	return (str[index]);
+}
 
 bool	check_pattern(char *filename, char *pattern)
 {
 	char	**strings;
 	int	index;
 	size_t	offset;
+	size_t	start;
 
 	index = 0;
 	offset = 0;
 	strings = ft_split(pattern, '*');
 	while (filename[offset] && strings[index])
 	{
-			if (strncmp(filename, strings[index], ft_strlen(strings[index])) == 0)
-				index++;
-			offset++;
+		if ((index == 0 && pattern[0] != '*') && strncmp(filename, strings[index], ft_strlen(strings[index])) != 0)
+			return (false);
+		start = ft_strlen(filename) - ft_strlen(strings[index]);
+		if (strings[index + 1] == NULL && get_last_char(pattern) != '*' && strncmp(filename + start, strings[index], ft_strlen(strings[index])) == 0)
+			return (true);
+		else if (strings[index + 1] == NULL && get_last_char(pattern) != '*')
+			return (false);
+		if (strncmp(filename + offset, strings[index], ft_strlen(strings[index])) == 0)
+		{
+			offset += ft_strlen(strings[index]);
+			index++;
+			continue ;
+		}
+		offset++;
 	}
+	int j;
+	j = 0;
 	if (strings[index] == NULL)
 		return (true);
 	return (false);
